5838045615a33b490b841048a1ade8b63e010d0d
kowalski <kowalski@Mac.lan>
2026-01-31 23:38:48 +0200

    entries table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,12 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     daily_goal INTEGER
 );
+
+CREATE TABLE entries (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    entry_date TEXT NOT NULL,
+    description TEXT,
+    calories INTEGER NOT NULL CHECK (calories > 0),
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
